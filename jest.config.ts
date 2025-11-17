9c3fc5fa24d7e2d55f37d5fc5f239dea8e4819e3
BinhNguyen <ntcbinh16@gmail.com>
2025-11-17 22:23:06 +0700

    feat: update jest config to ts

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,6 @@ import { createDefaultPreset } from 'ts-jest';
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
-/** @type {import("jest").Config} **/
-export const testEnvironment = 'node';
 export const transform = {
   ...tsJestTransformCfg,
 };
